67c2a0f3acca8ef10462f5d07dcf81710b198f97
Jean <46204992+JeanCarlosVR@users.noreply.github.com>
2021-03-30 21:38:43 -0500

    Delete src directory

--- src/internal/discord/bot/lib/utilities/additions/member/highestRole.ts
+++
@@ -1,8 +0,0 @@
-export default (_MoyaiLib: any) => {
-	Object.defineProperty(_MoyaiLib.Member.prototype, "highestRole", {
-		get: function() {
-			if(this.roles.length === 0) return this.guild.roles.get(this.guild.id);
-			else return this.roleObjects.reduce((prev, role) => !prev || role.higherThan(prev) ? role : prev);
-		}
-	});
-}
--- src/internal/discord/bot/lib/utilities/additions/member/roleObjects.ts
+++
@@ -1,7 +0,0 @@
-export default (_MoyaiLib: any) => {
-	Object.defineProperty(_MoyaiLib.Member.prototype, "roleObjects", {
-		get: function() {
-			return this.roles.map(roleID => this.guild.roles.get(roleID));
-		}
-	});
-}
